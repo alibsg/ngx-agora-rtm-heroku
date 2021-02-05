ca43d4efbdc6f553cbd123f2353748f46b7a8e6e
alibsg <alibsg@gmail.com>
2021-02-05 21:44:50 +0330

    adding node index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+import { RtmTokenBuilder, RtmRole } from 'agora-access-token';
+
+var PORT = process.env.PORT || 8080;
+
+if (!(process.env.APP_ID && process.env.APP_CERTIFICATE)) {
+    throw new Error('You must define an APP_ID and APP_CERTIFICATE');
+}
+var APP_ID = process.env.APP_ID;
+var APP_CERTIFICATE = process.env.APP_CERTIFICATE;
+
+var app = express();
+
+function nocache(req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next();
+}
+
+var generateAccessToken = function (req, resp) {
+    resp.header('Access-Control-Allow-Origin', "*")
+
+    var user = req.query.user;
+    if (!channel) {
+        return resp.status(500).json({ 'error': 'user name is required' });
+    }
+
+    const role = RtmRole.Rtm_User;
+    const expirationTimeInSeconds = 3600;
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
+    const token = RtmTokenBuilder.buildToken(APP_ID, APP_CERTIFICATE, user, role, privilegeExpiredTs);
+
+    return resp.json({ 'token': token });
+};
+
+app.get('/access_token', nocache, generateAccessToken);
+
+app.listen(PORT, function () {
+    console.log('Service URL http://127.0.0.1:' + PORT + "/");
+    console.log('Channel Key request, /access_token?uid=[user id]&channel=[channel name]');
+    console.log('Channel Key with expiring time request, /access_token?uid=[user id]&channel=[channel name]&expiredTs=[expire ts]');
+});
